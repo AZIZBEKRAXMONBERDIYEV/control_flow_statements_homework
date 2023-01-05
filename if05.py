a103f447faadcc29cf81d867405d6b4d4e7cd138
AZIZBEKRAXMONBERDIYEV <jxechkim079@gmail.com>
2023-01-05 21:57:38 +0500

    cod05

--- if05.py
+++ if05.py
@@ -8,4 +8,14 @@ def main(a,b,c):
     returns:
         integer: the number of negative numbers in the given numbers
     """
-    return
+    if a > 0 and b > 0 and c > 0 :
+        i = 0
+    if (a > 0 and b > 0 and c < 0) or (a < 0 and b > 0 and c > 0) or (a > 0 and b < 0 and c > 0) :
+        i = 1
+    if (a > 0 and b < 0 and c < 0) or (a < 0 and b > 0 and c < 0) or (a < 0 and b < 0 and c > 0) :
+        i = 2
+    if a < 0 and b < 0 and c < 0 :
+        i = 3
+        
+    return i
+print(main(6,4,-5))
